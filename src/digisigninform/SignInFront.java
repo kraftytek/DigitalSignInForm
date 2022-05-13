2742d631272e7bd10a450b5e655b5729077eb78d
kraftytek <61486981+kraftytek@users.noreply.github.com>
2022-05-13 11:26:17 -0700

    fixed connection issue
    fixed connection issue.

--- src/digisigninform/SignInFront.java
+++ src/digisigninform/SignInFront.java
@@ -37,7 +37,7 @@ public class SignInFront extends javax.swing.JFrame {
     }
 
     public String connectionUrl
-            = "jdbc:sqlserver://192.168.1.161;"
+            = "jdbc:sqlserver://192.168.1.161:1433;"
             + "encrypt=false;"
             + "databaseName=NCRO_WorkOrders;"
             + "user=appUser;"
